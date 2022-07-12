068763fc94165adb463522a54742fa90274c6857
AbhijeetSingh17032000 <108875043+AbhijeetSingh17032000@users.noreply.github.com>
2022-07-12 16:36:41 +0530

    Update Solution.cs

--- Linq HandsOn 2nd (12th July 2022)/Solution.cs
+++ Linq HandsOn 2nd (12th July 2022)/Solution.cs
@@ -71,6 +71,54 @@ namespace LINQ_HandsOn
     }
 }
 
+2. Find out Unique values
+//Main Function
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_HandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("How many string's you want to enter? ");
+            Console.WriteLine();
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            String[] b = new String[a];
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine("Enter " + (i + 1) + " record :");
+                b[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+
+            var result = (from studentData in b                         
+                          select studentData).Distinct().ToList();
+            var result1 = (from studentData in b
+                          select studentData).ToList();
+
+            Console.WriteLine("Unique (non-duplicate) strings :");
+            foreach (var c in result)
+            {
+                var QSCount = (from emp in result1
+                               where emp == c
+                               select emp).Count();
+                if(QSCount == 1)
+                Console.WriteLine(c);
+            }
+
+            Console.Read();
+        }
+    }
+}
+
 3. Top 5 numbers
 // Main Function
 using System;
