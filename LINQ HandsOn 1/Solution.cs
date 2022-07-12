d0997b3589b3a25119f10500fac7463af33abc04
AbhijeetSingh17032000 <108875043+AbhijeetSingh17032000@users.noreply.github.com>
2022-07-12 14:59:17 +0530

    Update Solution.cs

--- LINQ HandsOn 1/Solution.cs
+++ LINQ HandsOn 1/Solution.cs
@@ -298,4 +298,76 @@ namespace LINQ_HandsOn
 }
 
 8.
-	
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_HandsOn
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public Person(string Firstname, string Lastname, int Age)
+        {
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.Age = Age;
+        }
+        public static void HandsOn8()
+        {
+            var people = new List<Person>()
+        {
+            new Person("Bill",  "Smith", 41),
+            new Person("Sarah", "Jones", 22),
+            new Person("Stacy","Baker", 21),
+            new Person("Vivianne","Dexter", 19 ),
+            new Person("Bob","Smith", 49 ),
+            new Person("Brett","Baker", 51 ),
+            new Person("Mark","Parker", 19),
+            new Person("Alice","Thompson", 18),
+            new Person("Evelyn","Thompson", 58 ),
+            new Person("Mort","Martin", 58),
+            new Person("Eugene","DeLauter", 84 ),
+            new Person("Gail","Dawson", 19 ),
+            };
+
+            var result = (from TableName in people
+                         where TableName.Lastname.StartsWith("D")
+                         select TableName).Count();
+
+            Console.WriteLine(result);
+        }
+
+        internal class Linq
+            {
+
+            }
+    }
+}
+
+// Main Function
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_HandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person.HandsOn8();
+
+            Console.Read();
+        }
+    }
+}
+
+9.
